eca0f94153862b31aa6eab24dfa582ad7ca671c0
Israel <igarashiisrael@icloud.com>
2022-06-21 12:31:08 -0300

    Project created

---
+++ backend/src/main/java/com/igarashiisrael/pizzala/PizzaLaApplication.java
@@ -0,0 +1,13 @@
+package com.igarashiisrael.pizzala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaLaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaLaApplication.class, args);
+    }
+
+}
