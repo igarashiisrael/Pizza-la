86218d98ddadfa29a63354aff7150b5909b43acb
Israel <igarashiisrael@icloud.com>
2022-07-07 16:02:00 -0300

    Get-Products

--- backend/src/main/java/com/igarashiisrael/pizzala/repositories/ProductRepository.java
+++ backend/src/main/java/com/igarashiisrael/pizzala/repositories/ProductRepository.java
@@ -3,6 +3,10 @@ package com.igarashiisrael.pizzala.repositories;
 import com.igarashiisrael.pizzala.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository <Product, Long>{
+
+    List<Product> findAllByOrderByNameAsc();
 
 }
