295f821ed4d26e3eadee66d64a88b7811690100e
Israel <igarashiisrael@icloud.com>
2022-07-08 11:40:06 -0300

    GET / ORDERS

--- backend/src/main/java/com/igarashiisrael/pizzala/services/OrderService.java
+++ backend/src/main/java/com/igarashiisrael/pizzala/services/OrderService.java
@@ -1,7 +1,25 @@
 package com.igarashiisrael.pizzala.services;
 
+import com.igarashiisrael.pizzala.dto.OrderDTO;
+import com.igarashiisrael.pizzala.entities.Order;
+import com.igarashiisrael.pizzala.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<OrderDTO> findAll(){
+        List<Order> list = repository.findOrderWithProducts();
+        return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+    }
+
 }
